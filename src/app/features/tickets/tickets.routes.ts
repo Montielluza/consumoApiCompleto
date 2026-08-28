2e0df3e1328e986ed261f3724e660f18da167fea
Luza Montiel <luzamontiel20@gmail.com>
2026-08-28 04:09:40 -0500

    feat: add new and edit routes protected with roleGuard

--- src/app/features/tickets/tickets.routes.ts
+++ src/app/features/tickets/tickets.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 
+import { roleGuard } from '../../core/guards/role.guard';
+import { ROLES } from '../../core/constants/roles.constant';
+
 export const TICKETS_ROUTES: Routes = [
     {
         path: '',
         loadComponent: () => import('./ticket-list/ticket-list').then((m) => m.TicketList)
+    },
+    {
+        path: 'new',
+        canActivate: [roleGuard([ROLES.ADMIN, ROLES.CLIENT])],
+        loadComponent: () => import('./ticket-form/ticket-form').then((m) => m.TicketForm)
+    },
+    {
+        path: ':id/edit',
+        canActivate: [roleGuard([ROLES.ADMIN, ROLES.AGENT])],
+        loadComponent: () => import('./ticket-edit/ticket-edit').then((m) => m.TicketEdit)
     },
     {
         path: ':id',
